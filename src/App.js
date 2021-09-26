92e934b9e9087ac15f57899e2f4b2fdf8178d98a
Guilherme Araujo Oliveira <guilhermearaujooliveira4@gmail.com>
2021-09-26 00:00:42 -0300

    v0.2

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
-import { Route, Router, Switch } from 'react-router';
-import { Link } from 'react-router-dom';
-import Data from "./components/Backend/Data/Data"
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import './components/css/Header.css';
 import './App.css';
 
-import Header from './Frontend/Header';
-import About from './components/Frontend/About';
+import CartImg from './images/shopping-cart-solid.svg';
+
+
+import About from './components/About.jsx';
+import Contact from './components/Contact.jsx';
+import LoginRegister from './components/LoginRegister.jsx';
+import Cart from './components/Cart';
 
 
 
 function App() {
-  const { produtos } = Data;
+  // const { produtos } = Data;
   return (
     <Router>
-      <div className="App">
-          <Header/>
-
-        <Switch>
-          <Route path="/about">
-            <About/>
-          </Route>
-        </Switch>
-      </div>
+      <div>
+      <header className="header">
+            <Link to="/"><h1>Amazon</h1></Link>
+            <aside>
+                <ul>
+                    <Link to="/"><li>Home</li></Link>
+                    <Link to="/about"><li>About</li></Link>
+                    <Link to="/contact"><li>Contact</li></Link>
+                    <Link to="/loginResgister"><li>Login - Register</li></Link>
+                </ul>
+            </aside>
+                    <Link to="/cart"><img src={CartImg} alt="Carrinho de compras" className="cart"/></Link>
+                    <b>0</b>
+        </header>
+        
+          <Switch>
+            <Route path="/about" exact component={About}/>
+            <Route path="/contact" exact component={Contact}/>
+            <Route path="/loginResgister" exact component={LoginRegister}/>
+            <Route path="/cart" exact component={Cart}/>
+          </Switch>
+      </div>      
     </Router>
-  );
+
+  )
 }
 
 export default App;
---
+++ src/components/LoginRegister.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoginRegister = () => {
+    return (
+        <div>
+            login page
+        </div>
+    )
+}
+
+export default LoginRegister;
